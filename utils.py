b44fcf131395f745633fd36aa6c2df8639e1373d
ahany42 <ahanyfathy42@gmail.com>
2025-03-12 15:38:01 +0200

    file structure completed

--- utils.py
+++ utils.py
@@ -4,11 +4,10 @@ def confusion_matrix():
 def accuracy_score():
     pass
 
-def train_test_split():
-    pass
-
-def train_test_samples(df):
-
+def train_test_samples():
+    #Use the birds data in both your training and testing processes. 
+    # (Each class has 50 samples: train NN with 30 non-repeated samples randomly 
+    # selected, and test it with the remaining 20 samples)
     pass;
 
 def linear_activation_fn(x):
@@ -18,4 +17,9 @@ def signum_activation_fn(x):
     return 1 if x > 0 else -1 if x < 0 else 0
 
 def encoding():
+    # from c1 , c2 to 1 and -1 or 0 1 according to activation fn
+    pass
+
+def input_output_mapping():
+    #from labels to c1 c2 again
     pass
