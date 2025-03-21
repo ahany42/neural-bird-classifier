b77dd20dd8a11c630607ac84aea3ed366e7f0a28
ahany42 <ahanyfathy42@gmail.com>
2025-03-21 17:27:31 +0200

    test slp

--- slp.py
+++ slp.py
@@ -6,12 +6,15 @@ def main(feature1, feature2, class1, class2, eta, epochs, bias):
     print(feature1, feature2, class1, class2, eta, epochs, bias, sep="  ")
     X_train, y_train, X_test, y_test = utils.preprocessing(feature1, feature2, class1, class2)
     weights = train(X_train, y_train, eta, epochs, bias)
-    y_pred = predict(X_test, weights, bias)
+    y_pred= test(X_test,y_test,weights, bias)
+
     print("True Labels (Encoded):", y_test)
     print("Predicted Labels (Encoded):", y_pred)
     accuracy ,TP,FP,FN,TN = utils.evaluate(class1,class2,y_test, y_pred)
     return accuracy ,TP,FP,FN,TN
-
+def test (X_test,y_test,weights, bias):
+    y_pred = predict(X_test,weights,bias)
+    return y_pred 
 def train(X_train, y_train, eta, epochs, bias):
     np.random.seed(42)
     X_train = np.array(X_train, dtype=np.float64)
